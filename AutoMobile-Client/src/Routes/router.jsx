4edf686fa701b4eccb2ab278d31fd9b6ba3619ee
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-10-03 04:06:11 +0600

    first commit

---
+++ AutoMobile-Client/src/Routes/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+        {
+            path: "/", 
+            element: <Home></Home>, 
+        }
+    ]
+  },
+]);
